5b972e919d9dcb169009648e14f9c4e0cdbffc9f
Florian Traverse <florian.traverse@gmail.com>
2011-07-25 15:35:48 +0200

    added some tests for negative/very short timeout -> 0.2.1

--- test/test.js
+++ test/test.js
@@ -26,6 +26,50 @@ vows.describe('addTimeout').addBatch({
        }
       }
     },
+    'with a negative timeout (-500)': {
+     'and a callback that is called': {
+      '*as soon as possible* (synchronously)': {
+        topic: function () {
+          var cb = this.callback;
+          addTimeout(-500, cb);
+        },
+        'we get *an* error (this delay is unereachable)': function (err) {
+            assert.instanceOf(err, addTimeout.TimeoutError);
+        }
+      },
+      '*after* timeout (10ms)': {
+        topic: function () {
+          var cb = this.callback;
+        	setTimeout(addTimeout(-500, cb), 10);
+        },
+        'we get an error!': function (err) {
+          assert.instanceOf(err, addTimeout.TimeoutError);
+        }
+       }
+      }
+    },
+    'with a short timeout of 1ms (shorter than what is able to prive setTimeout)': {
+     'and a callback that is called': {
+      '*before* timeout (synchronously)': {
+        topic: function () {
+          var cb = this.callback;
+          (addTimeout(1, cb))();
+        },
+        'we get no error': function (err) {
+            assert.isUndefined (err);
+        }
+      },
+      '*after* timeout (2ms)': {
+        topic: function () {
+          var cb = this.callback;
+        	setTimeout(addTimeout(1, cb), 2);
+        },
+        'we get an error!': function (err) {
+          assert.instanceOf(err, addTimeout.TimeoutError);
+        }
+       }
+      }
+    },
     'with an *error handler* for a timeout of 500ms': {
      'and a callback that is called': {
        '*before* timeout (100ms)': {
